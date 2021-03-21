3595f4811de805a8ae5c56bc5b9a487e9b9c9428
Saputro-bit <muhammadnoorsaputro23@gmail.com>
2021-03-21 18:55:18 +0700

    Update Activity

---
+++ app/src/main/java/com/example/myapplication/ActivityHasil.java
@@ -0,0 +1,40 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class ActivityHasil extends AppCompatActivity {
+    //Mendeklarasikan variabel dengan tipe data TextView
+    TextView txEmail, txPassword;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hasil);
+
+        //menghubungkan variabel txEmail dengan componen TextView pada layout
+        txEmail = findViewById(R.id.tvEmail);
+
+        //menghubungkan variabel txPassword dengan componen TextView pada layout
+        txPassword = findViewById(R.id.tvPassword);
+
+        //membuat objek bundle dan mengambil data yang dikirimkan dari activity sebelumnya
+        Bundle bundle = getIntent().getExtras();
+
+        //membuat variabel string yang digunakan untuk menyimpan data yang
+        //dikirimkan dari activity sebelumnya dengan kunci "a"
+        String email = bundle.getString( "a");
+
+        //membuat variabel string yang digunakan untuk menyimpan data yang
+        //dikirimkan dari activity sebelumnya dengan kunci "b"
+        String pass = bundle.getString("b");
+
+        //menampilkan value dari variabel email kedalam txEmail
+        txEmail.setText(email);
+
+        //menampilkan value dari variabel pass kedalam txPassword
+        txPassword.setText(pass);
+    }
+}
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,13 +2,103 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    //deklarasi variabel untuk Button
+    Button btnLogin;
+
+    //Deklarasi variabel untuk EditText
+    EditText edemail, edpassword;
+
+    //Deklarasi variabel untuk menyimpan email dan password
+    String nama, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Menghubungkan variabel btnLogin dengan componen button pada Layout
+        btnLogin=findViewById(R.id.btsignIn);
+
+        //Menghubungkan variabel edemail dengan componen button pada Layout
+        edemail=findViewById(R.id.edEmail);
+
+        //Menghubungkan variabel edpassword dengan componen button pada Layout
+        edpassword=findViewById(R.id.edPassword);
+
+        //membuat fingfi onclik pada button btnLogin
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //menyimpan input user di edittext email ke dalam variabel nama
+                nama = edemail.getText().toString();
+
+                //menyimpan input user di edittext password ke dalam variabel nama
+                password = edemail.getText().toString();
+
+                //menengeset email yang benar
+                String email = "muhammadnoorsaputro23@mail.com";
+                //meneset password yang benar
+                String pass = "123456";
+
+                //mengecek apakahh edittext email dan password terdapat isi atau tidak
+                if (nama.isEmpty() || password.isEmpty()) {
+                    //membuat variabel toast dan menampilkan pesan "Login Sukse"
+                    Toast t = Toast.makeText(getApplicationContext(),
+                            "Login Sukses",
+                            Toast.LENGTH_LONG);
+                    //menampilan toast
+                    t.show();
+                } else {
+
+                    //mengecek apakaj isi dari rmail dan password sudah sama dengan email dan
+                    //password yang sudah di set
+                    if (nama.equals(email) && password.equals(pass)) {
+                        //membuat variabel toast dan menampilkan pesan "login Sukses"
+                        Toast t = Toast.makeText(getApplicationContext(),
+                                "Login sukse",
+                                Toast.LENGTH_LONG);
+                        //menampilkan toast
+                        t.show();
+                        //membuat objek bundle
+                        Bundle b = new Bundle();
+
+                        //memasukkan value dari variabel nama kunci "a"
+                        //dan dimasukkan ke dalam bundle
+                        b.putString("a", nama.trim());
+
+                        //memasukkan value dari variabel password dengan kunci "b"
+                        //dan dimaasukkan kedalam bundle
+                        b.putString("b", password.trim());
+
+                        //membuat objek intent berpindah activity dari mainactivity ke ActivityHasil
+                        Intent i = new Intent(getApplicationContext(), ActivityHasil.class);
+
+                        //memasukkan bundle kedalam intent untuk dikirimkan ke ActivityHasil
+                        i.putExtras(b);
+                        //berpindah ke ActivityHasil
+                        startActivity(i);
+                    } else {
+                        //membuat variabel toast dan membut toast dan menampilkan pesan "Login Sukses"
+                        Toast t = Toast.makeText(getApplicationContext(),
+                                "Login Gagal", Toast.LENGTH_LONG);
+                        //menampilkan toast
+                        t.show();
+                    }
+                }
+            }
+        });
     }
 }
+
+
+
+
