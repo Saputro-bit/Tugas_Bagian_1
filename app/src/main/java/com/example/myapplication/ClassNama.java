3ddbe3a720277908f20282ea4ca5ae0552153913
Saputro-bit <muhammadnoorsaputro23@gmail.com>
2021-03-31 15:25:15 +0700

    Tugas Pertemuan Tanggal 29

---
+++ app/src/main/java/com/example/myapplication/ClassNama.java
@@ -0,0 +1,17 @@
+package com.example.myapplication;
+
+public class ClassNama {
+    private String Nama;
+
+    //inisialisasi objek
+    public ClassNama(String nama)
+    {
+        this.Nama = nama;
+    }
+
+    //Membuuat method getName untuk mengembalikan nilai le variabel Nama
+    public String getNama()
+    {
+        return this.Nama;
+    }
+}
